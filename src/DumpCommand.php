fd7484e3bcbcd6720d97a3c769b13d0936263559
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-04-07 22:18:16 +0200

    Add built-in support for artisan and deployer

--- src/DumpCommand.php
+++ src/DumpCommand.php
@@ -53,6 +53,8 @@ class DumpCommand extends Command
                     'phpspec',
                     'behat',
                     'robo',
+                    'dep',
+                    'artisan',
                 );
             }
 
